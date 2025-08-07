b50200e1f8caec31399415126ca49fbe022529cb
AliRamdhan <aliramdhan4551@gmail.com>
2025-08-07 15:59:32 +0700

    [feat] add template product 1

--- client/src/app/templates/[slug]/page.tsx
+++ client/src/app/templates/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TemplateCreative1Wrapper from "@/components/templates-profile/creatives/templates-creative-1/templates-creative1-wrapper";
 import TemplatesCreative2Wrapper from "@/components/templates-profile/creatives/templates-creative-2/templates-creative2-wrapper";
+import TemplatesProduct1Wrapper from "@/components/templates-profile/products/templates-product-1/templates-product1-wrapper";
 
 const Page = ({ params }: { params: { slug: string } }) => {
   const slug = params.slug;
@@ -9,6 +10,7 @@ const Page = ({ params }: { params: { slug: string } }) => {
     <section>
       {slug === "project-cyberscape" && <TemplateCreative1Wrapper />}
       {slug === "ethereal-threads" && <TemplatesCreative2Wrapper />}
+      {slug === "quantum-leap" && <TemplatesProduct1Wrapper />}
     </section>
   );
 };
