3da95e48d73ae88ce88a633e08380fa44ae5a80e
AliRamdhan <aliramdhan4551@gmail.com>
2025-09-02 19:39:53 +0700

    [fix] fix link data of showcases , navigation hover and product card of sudut profile construction

--- showcases/src/app/sudut-profile-construction/_components/products-wrapper.tsx
+++ showcases/src/app/sudut-profile-construction/_components/products-wrapper.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import TransitionLink from "@/components/shared/transition-link";
 import { productsData } from "../lib/data";
+import ProductCard from "./cards/product_card";
 
 export function Products() {
   return (
@@ -17,40 +17,7 @@ export function Products() {
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-16">
           {productsData.slice(0, 4).map((product) => (
-            <div
-              key={product.label}
-              className="bg-background h-full rounded-md shadow-lg overflow-hidden hover-lift transition-all duration-300 ease-in-out border-2 border-transparent hover:border-primary/10"
-            >
-              <div className="relative h-64 overflow-hidden">
-                <Image
-                  src={product.image}
-                  alt={product.label}
-                  fill
-                  className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
-                />
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 transition-opacity duration-300">
-                  <p className="text-white text-center px-4">
-                    {product.description}
-                  </p>
-                </div>
-              </div>
-              <div className="p-6 h-48 flex flex-col justify-between items-center">
-                <div>
-                  <h3 className="text-xl font-semibold text-foreground mb-2">
-                    {product.label}
-                  </h3>
-                  <p className="text-gray-700 text-sm mb-2">{product.price}</p>
-                </div>
-                <div className="flex space-x-2 w-full">
-                  <Button
-                    variant="outline"
-                    className="flex-1 border-gray-400 text-gray-700 hover:bg-gray-100 bg-transparent"
-                  >
-                    Consult & Order Here
-                  </Button>
-                </div>
-              </div>
-            </div>
+            <ProductCard key={product.label} product={product} />
           ))}
         </div>
         <TransitionLink href={"sudut-profile-construction/products"}>
--- showcases/src/app/sudut-profile-construction/products/page.tsx
+++ showcases/src/app/sudut-profile-construction/products/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import { productsData } from "../lib/data";
+import ProductCard from "../_components/cards/product_card";
 
 const Page = () => {
   return (
@@ -18,42 +17,7 @@ const Page = () => {
           </p>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mb-12">
             {productsData.map((product) => (
-              <div
-                key={product.label}
-                className="bg-background h-full lg:h-[48vh] rounded-md shadow-lg overflow-hidden hover-lift transition-all duration-300 ease-in-out border-2 border-transparent hover:border-primary/10"
-              >
-                <div className="relative h-64 overflow-hidden">
-                  <Image
-                    src={product.image}
-                    alt={product.label}
-                    fill
-                    className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
-                  />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 transition-opacity duration-300">
-                    <p className="text-white text-center px-4">
-                      {product.description}
-                    </p>
-                  </div>
-                </div>
-                <div className="p-6 h-48 flex flex-col justify-between items-center">
-                  <div>
-                    <h3 className="text-xl font-semibold text-foreground mb-2">
-                      {product.label}
-                    </h3>
-                    <p className="text-gray-700 text-sm mb-2">
-                      {product.price}
-                    </p>
-                  </div>
-                  <div className="flex space-x-2 w-full">
-                    <Button
-                      variant="outline"
-                      className="flex-1 border-gray-400 text-gray-700 hover:bg-gray-100 bg-transparent"
-                    >
-                      Consult & Order Here
-                    </Button>
-                  </div>
-                </div>
-              </div>
+              <ProductCard key={product.label} product={product} />
             ))}
           </div>
         </div>
